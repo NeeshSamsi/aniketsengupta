45d5967ed57df2b781b82ba730f35168ecf8fa4c
Neesh <avaneeshsamsi@gmail.com>
2024-10-09 21:28:09 +0200

    fix: Uppercase eyebrow text

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -17,7 +17,7 @@ export default function Section({
     <section className={className}>
       <div className="container mx-auto grid grid-cols-1 gap-8 px-8 py-8 sm:grid-cols-[1fr,4fr] md:gap-12 lg:py-24 xl:py-32">
         <div className="grid grid-cols-[subgrid] gap-1 sm:col-span-2 sm:gap-4 md:gap-6">
-          <h2 className="text-xs font-medium sm:text-sm lg:text-base">
+          <h2 className="text-xs font-medium uppercase sm:text-sm lg:text-base">
             {subheading}
           </h2>
           <p className="font-serif text-3xl sm:text-4xl lg:text-5xl">
