e1593535b20871640287fb6fb7913b9d69a90266
Neesh <avaneeshsamsi@gmail.com>
2024-10-09 11:53:39 +0200

    feat: Responsive hero slice

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,5 +1,7 @@
-import { Content } from "@prismicio/client"
+import { asText, Content } from "@prismicio/client"
+import { PrismicNextImage } from "@prismicio/next"
 import { SliceComponentProps } from "@prismicio/react"
+import Button from "@/components/Button"
 
 /**
  * Props for `Hero`.
@@ -10,12 +12,34 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>
  * Component for "Hero" Slices.
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
+  const {
+    primary: { bgImage, heading, paragraph, button },
+  } = slice
+
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="grid-stack max-h-screen items-end"
     >
-      Placeholder component for hero (variation: {slice.variation}) Slices
+      <PrismicNextImage
+        field={bgImage}
+        alt=""
+        className="h-full w-full object-cover"
+      />
+
+      <div className="container mx-auto px-8 py-8 text-primary-100 lg:py-12 xl:py-24">
+        <p className="max-w-[50ch] text-pretty text-base sm:text-lg lg:text-xl">
+          {asText(paragraph)}
+        </p>
+        <h1 className="mt-2 text-balance font-serif text-4xl leading-[1.1] sm:text-5xl md:text-6xl lg:text-7xl xl:mt-3">
+          {asText(heading)}
+        </h1>
+
+        <Button link={button} size="base" className="mt-6 xl:mt-10">
+          {button.text}
+        </Button>
+      </div>
     </section>
   )
 }
