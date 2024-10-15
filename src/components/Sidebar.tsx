e5e504e6ad1676a4ab4b8de829636e39b3ae31d5
Neesh <avaneeshsamsi@gmail.com>
2024-10-15 12:40:13 +0200

    feat: Allow onClick prop

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,20 +5,23 @@ export function SidebarItem({
   text,
   selected,
   className,
+  onClick,
 }: {
   text: KeyTextField | string
   selected: boolean
   className?: string
+  onClick?: () => void
 }) {
   return (
     <button
       className={cn(
-        "transition-all hover:font-medium hover:text-primary-800",
+        "transition-all duration-300 hover:font-medium hover:text-primary-800",
         className,
         {
           "font-semibold text-primary-800 hover:font-semibold": selected,
         },
       )}
+      onClick={onClick}
     >
       {selected && <span>• </span>}
       {text}
